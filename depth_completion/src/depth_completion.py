Fixed validation evaluation mask and added normalization if not normalized

depth_completion/src/depth_completion.py
@@ -933,11 +933,11 @@ def validate(depth_model,
             # bottom crop
             end_y = height
             start_y = end_y - crop_height
-            output_depth = output_depth[..., start_y:end_y, start_x:end_x]
-            ground_truth = ground_truth[..., start_y:end_y, start_x:end_x]
+            output_depth = output_depth[start_y:end_y, start_x:end_x]
+            ground_truth = ground_truth[start_y:end_y, start_x:end_x]
 
         # Select valid regions to evaluate
-        validity_mask = np.where(np.squeeze(validity_map.cpu().numpy()) > 0, 1, 0)
+        validity_mask = np.where(ground_truth > 0, 1, 0)
 
         min_max_mask = np.logical_and(
             ground_truth > min_evaluate_depth,

depth_completion/src/fusionnet_models.py
@@ -133,8 +133,9 @@ class FusionNetModel(object):
                 sparse_depth=sparse_depth,
                 validity_map=validity_map)
 
-        # Normalize images between [0, 1]
-        image = image / 255.0
+        # Normalize image between [0, 1]
+        if torch.max(image) > 1.0:
+            image = image / 255.0
 
         return image, filtered_sparse_depth, filtered_validity_map
 
@@ -201,6 +202,11 @@ class FusionNetModel(object):
         '''
 
         assert self.model_pose is not None
+
+        # Normalize image between [0, 1]
+        image0, image1 = [
+            image / 255.0 for image in [image0, image1] if torch.max(image) > 1.0
+        ]
 
         return self.model_pose.forward(image0, image1)
 
@@ -280,7 +286,7 @@ class FusionNetModel(object):
 
         # Normalize images between [0, 1]
         image0, image1, image2 = [
-            image / 255.0 for image in [image0, image1, image2]
+            image / 255.0 for image in [image0, image1, image2] if torch.max(image) > 1.0
         ]
 
         # Compute loss function

depth_completion/src/voiced_models.py
@@ -80,7 +80,8 @@ class VOICEDModel(object):
         '''
 
         # Normalize image between [0, 1]
-        image = image / 255.0
+        if torch.max(image) > 1.0:
+            image = image / 255.0
 
         return image, sparse_depth, validity_map
 
@@ -132,6 +133,11 @@ class VOICEDModel(object):
         '''
 
         assert self.model_pose is not None
+
+        # Normalize image between [0, 1]
+        image0, image1 = [
+            image / 255.0 for image in [image0, image1] if torch.max(image) > 1.0
+        ]
 
         return self.model_pose.forward(image0, image1)
 
@@ -206,7 +212,7 @@ class VOICEDModel(object):
 
         # Normalize images between [0, 1]
         image0, image1, image2 = [
-            image / 255.0 for image in [image0, image1, image2]
+            image / 255.0 for image in [image0, image1, image2] if torch.max(image) > 1.0
         ]
 
         # Compute pose from 1 to 0 and 2 to 0 for pose consistency loss