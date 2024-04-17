e71d4859955660f61eee4564ce935276aec9bdc1
mcchu1217 <michael.chu@yale.edu>
2024-04-16 21:31:09 -0400

    finished ewc

--- depth_completion/src/losses.py
+++ depth_completion/src/losses.py
@@ -2,22 +2,6 @@ import torch
 
 # TODO: Add losses here as functions (e.g., EWC, LWF)
 
-def compute_fisher(fisher_info, params, normalization):
-    '''
-    Calculate the fisher information matrix for a model's parameters on a task
-
-    Arg(s):
-
-    Returns:
-        Fisher information matrix
-    '''
-
-    for idx, param in enumerate(params):
-        if param.grad is not None:
-            fisher_info[idx] += param.grad.data ** 2 / normalization
-
-    return fisher_info
-
 
 def ewc_loss(current_params, frozen_params, fisher_info, lambda_ewc):
     '''
@@ -27,7 +11,8 @@ def ewc_loss(current_params, frozen_params, fisher_info, lambda_ewc):
         current_params : list[torch.Tensor[float32]]
         froze_params : list[torch.Tensor[float32]]
     Returns:
-
+        loss : torch.Tensor[float32]
+            EWC loss
     '''
     loss = 0.0
 
