e806dda9d85c20fc3bcbbf1d0d9af0a06e176e8b
Patrick Rim <patrickqrim@gmail.com>
2024-04-22 16:34:34 -0400

    Added support for multiple min/max eval DEPTHS

--- depth_completion/bash/train_kbnet-void1500-kitti.sh
+++ depth_completion/bash/train_kbnet-void1500-kitti.sh
@@ -12,10 +12,18 @@ python depth_completion/src/train_depth_completion.py \
 --train_intrinsics_path \
     training/void/unsupervised/void_train_intrinsics_1500.txt \
     training/kitti/unsupervised/kitti_train_nonstatic_intrinsics.txt \
---val_image_paths testing/void/void_test_image_1500.txt \
---val_sparse_depth_paths testing/void/void_test_sparse_depth_1500.txt \
---val_intrinsics_paths testing/void/void_test_intrinsics_1500.txt \
---val_ground_truth_paths testing/void/void_test_ground_truth_1500.txt \
+--val_image_paths \
+    testing/void/void_test_image_1500.txt \
+    validation/kitti/kitti_val_image.txt \
+--val_sparse_depth_paths \
+    testing/void/void_test_sparse_depth_1500.txt \
+    validation/kitti/kitti_val_sparse_depth.txt \
+--val_intrinsics_paths \
+    testing/void/void_test_intrinsics_1500.txt \
+    validation/kitti/kitti_val_intrinsics.txt \
+--val_ground_truth_paths \
+    testing/void/void_test_ground_truth_1500.txt \
+    validation/kitti/kitti_val_ground_truth.txt \
 --model_name kbnet_void \
 --network_modules depth pose \
 --min_predict_depth 0.1 \
@@ -54,13 +62,13 @@ python depth_completion/src/train_depth_completion.py \
     w_smoothness=2.0 \
     w_weight_decay_depth=0.0 \
     w_weight_decay_pose=0.0 \
---min_evaluate_depth 0.2 \
---max_evaluate_depth 5.0 \
+--min_evaluate_depths 0.2 0.0 \
+--max_evaluate_depths 5.0 100.0 \
 --evaluation_protocol default \
 --n_step_per_summary 1000 \
 --n_step_per_checkpoint 1000 \
 --start_step_validation 100000 \
 --checkpoint_path \
 trained_completion/kbnet/void1500_kitti/kbnet_12x416x512_min01max80_lr0-1e4_20-5e5_40_co015_st095_sz200_sm200_bri050-150_con050-150_hue10_sat050-150_hvflip_rot25_cap090-100_rap060-100_rac000-000_rmim1e3-5e3_5x5_rmpt060-070 \
---device cpu \
+--device gpu \
 --n_thread 8
