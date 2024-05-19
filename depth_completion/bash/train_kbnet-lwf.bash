a92db7499e43aabbbc333fd0eda32c2f7ea3cb37
xien_chen <xien.chen@yale.edu>
2024-05-19 17:20:11 -0400

    bash scripts and oop

--- depth_completion/bash/train_kbnet-lwf.bash
+++ depth_completion/bash/train_kbnet-lwf.bash
@@ -5,10 +5,13 @@ export CUDA_VISIBLE_DEVICES=0
 python depth_completion/src/train_depth_completion.py \
 --train_image_paths \
     /media/home/alwong/workspace/continual-depth-completion/training/void/unsupervised/void_train_image_1500.txt  \
+    /media/home/alwong/workspace/continual-depth-completion/training/nyu_v2/unsupervised/nyu_v2_train_image_uniform.txt  \
 --train_sparse_depth_path \
     /media/home/alwong/workspace/continual-depth-completion/training/void/unsupervised/void_train_sparse_depth_1500.txt   \
+    /media/home/alwong/workspace/continual-depth-completion/training/nyu_v2/unsupervised/nyu_v2_train_sparse_depth_uniform.txt  \
 --train_intrinsics_path \
     /media/home/alwong/workspace/continual-depth-completion/training/void/unsupervised/void_train_intrinsics_1500.txt  \
+    /media/home/alwong/workspace/continual-depth-completion/training/nyu_v2/unsupervised/nyu_v2_train_intrinsics_uniform.txt \
 --val_image_path /media/home/alwong/workspace/continual-depth-completion/testing/void/void_test_image_1500.txt   \
 --val_sparse_depth_path /media/home/alwong/workspace/continual-depth-completion/testing/void/void_test_sparse_depth_1500.txt \
 --val_intrinsics_path /media/home/alwong/workspace/continual-depth-completion/testing/void/void_test_intrinsics_1500.txt \
@@ -16,10 +19,11 @@ python depth_completion/src/train_depth_completion.py \
 --model_name kbnet_void \
 --network_modules depth pose \
 --min_predict_depth 0.1 \
---max_predict_depth 8.0 \
+--max_predict_depth 100.0 \
 --train_batch_size 12 \
 --train_crop_shapes \
     416 512 \
+    320 768 \
 --learning_rates 1e-4 5e-5 \
 --learning_schedule 20 40 \
 --augmentation_probabilities 1.0 \
@@ -50,6 +54,7 @@ python depth_completion/src/train_depth_completion.py \
     w_smoothness=2.0 \
     w_weight_decay_depth=0.0 \
     w_weight_decay_pose=0.0 \
+    w_lwf=0.5\
 --min_evaluate_depth 0.2 \
 --max_evaluate_depth 5.0 \
 --evaluation_protocol default \
@@ -57,6 +62,6 @@ python depth_completion/src/train_depth_completion.py \
 --n_step_per_checkpoint 1000 \
 --start_step_validation 100000 \
 --checkpoint_path \
-trained_completion/kbnet/void1500/kbnet_12x416x512_min01max80_lr0-1e4_20-5e5_40_co015_st095_sz200_sm200_bri050-150_con050-150_hue10_sat050-150_hvflip_rot25_cap090-100_rap060-100_rac000-000_rmim1e3-5e3_5x5_rmpt060-070 \
+trained_completion/kbnet/void1500_nyu/kbnet_12x416x512_min01max80_lr0-1e4_20-5e5_40_co015_st095_sz200_sm200_bri050-150_con050-150_hue10_sat050-150_hvflip_rot25_cap090-100_rap060-100_rac000-000_rmim1e3-5e3_5x5_rmpt060-070 \
 --device gpu \
 --n_thread 8
--- depth_completion/src/losses.py
+++ depth_completion/src/losses.py
@@ -1,32 +1,29 @@
 import torch
 import torch.nn.functional as F
 
-#Before updating the model with the new task data, you run the new task data through the model (as it was trained on the old tasks) and record its predictions. These predictions serve as a stand-in for the actual ground truth of the old tasks, which you do not have. 
-#During training on the new task, you aim to find a set of parameters for the model that not only performs well on the new task but also produces similar predictions to the recorded predictions by the frozen model. 
-def compute_loss(self,
-                output_depth0,
-                output_frozen_depth0,
-                lambda_lwf):
-    '''
-    Compute the LWF loss for unsupervised learning scenarios based on depth prediction.
+class lwf_loss:
+    def __init__(self):
+        """
+        Initialize the Learning without Forgetting (LwF) loss class.
+        """
+        pass
 
-    Args:
-        output_depth0 : torch.Tensor
-            current model's output depth maps [N, 1, H, W].
-        output_frozen_depth0 : torch.Tensor[float32]
-                frozen model's output depth maps N x 1 x H x W 
-        lambda_lwf : float
-            Regularization weight for the LWF loss component.
+    def __call__(self, output_depth0, output_frozen_depth0, lambda_lwf):
+        """
+        Compute the LwF loss for unsupervised learning scenarios based on depth prediction.
 
-    Returns:
-        torch.Tensor : The computed LWF loss, scaled by lambda_lwf\.
-    '''
+        Args:
+            output_depth0 (torch.Tensor): Current model's output depth maps [N, 1, H, W].
+            output_frozen_depth0 (torch.Tensor): Frozen model's output depth maps [N, 1, H, W].
+            lambda_lwf (float): Regularization weight for the LwF loss component.
 
-    # lwf LOSS: the difference between current and frozen model outputs. (original paper uses knoweldge distillation loss for classification)
-    lwf_loss = F.mse_loss(output_depth0, output_frozen_depth0)
-    
-    # Weighted sum of the current depth loss and the LWF loss.
-    combined_loss = lambda_lwf * lwf_loss
-    
-    return combined_loss
+        Returns:
+            torch.Tensor: The computed LwF loss, scaled by lambda_lwf.
+        """
+        # Calculate the MSE loss between current and frozen model outputs
+        lwf_loss = F.mse_loss(output_depth0, output_frozen_depth0)
 
+        # Weight the loss by lambda_lwf
+        combined_loss = lambda_lwf * lwf_loss
+
+        return combined_loss
