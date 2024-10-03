d8aae50032d5d434ddcd958e2bd0d48c55748f78
Patrick Rim <patrickqrim@gmail.com>
2024-10-02 22:29:35 -0400

    Added support for multiple evaluation protocols

--- depth_completion/src/train_depth_completion.py
+++ depth_completion/src/train_depth_completion.py
@@ -142,7 +142,7 @@ parser.add_argument('--min_evaluate_depths',
 parser.add_argument('--max_evaluate_depths',
     nargs='+', type=float, default=[5.00], help='Maximum value of depth to evaluate')
 parser.add_argument('--evaluation_protocol',
-    type=str, default='default', help='Protocol for evaluation i.e. vkitti, nuscenes, default')
+    nargs='+', type=str, default=['default'], help='Protocol for evaluation i.e. vkitti, nuscenes, default')
 
 # Checkpoint settings
 parser.add_argument('--checkpoint_path',
